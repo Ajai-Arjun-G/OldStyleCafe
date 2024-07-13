94fe7bc2d92841f4e8d86993dcbe1d88bf7c28d6
Ajai Arjun <ajaiarjun2003@gmail.com>
2024-07-13 10:20:22 +0530

    Added start screen and game states

--- main.py
+++ main.py
@@ -15,8 +15,12 @@ surface.fill('#87CEEB')
 
 # Font
 font_path = 'D:/Term - 4/Pygame/font/Margarine/Margarine-Regular.ttf'
+fontmain = 'D:/Term - 4/Pygame/font/Ashford/ashfo.ttf'
 font_size = 35
 custom_font = pygame.font.Font(font_path, font_size)
+startFont = pygame.font.Font(fontmain, 40)
+
+
 
 # Clouds in the Background - Layer 2
 cloudSurface = pygame.image.load('D:/Term - 4/Pygame/Clouds/cloud.png').convert_alpha()
@@ -149,6 +153,23 @@ keyT = pygame.transform.smoothscale(keyT,(40,40))
 slash = pygame.image.load('D:/Term - 4/Pygame/furniture/slash.png').convert_alpha()
 slash = pygame.transform.smoothscale(slash,(35,35))
 
+startSurface = pygame.Surface(displaySize)
+startSurface.fill('#87CEEB')
+
+startKey = pygame.image.load('D:/Term - 4/Pygame/start screen/enter.png').convert_alpha()
+startKey = pygame.transform.smoothscale(startKey,(110,90))
+
+# Fill the screen with a blue color
+screen.fill('#87CEEB')
+
+#Title
+instructions_surface = custom_font.render("Press", True, (255, 255, 255))
+instructions_surface2 = custom_font.render("to start", True, (255, 255, 255))
+
+#logo 
+logo = pygame.image.load('D:/Term - 4/Pygame/start screen/Brews &.png').convert_alpha()
+logo = pygame.transform.smoothscale(logo,(550,550))
+
 # Rectangle of Tables
 table1_rect = tableSurface.get_rect(midleft=(-30, 700))
 table2_rect = tableSurface2.get_rect(midleft=(450, 785))
@@ -173,8 +194,8 @@ toaster_rect = toast.get_rect(midbottom=(1100, 710))
 coffeeM_rect = coffeemaker.get_rect(midbottom=(400, 700))
 
 # Rectangle for food
-bread_rect = bread.get_rect(midbottom=(780, 670))
-coffee_rect = coffee.get_rect(midbottom=(780, 670))
+bread_rect = bread.get_rect(midbottom=(780, 685))
+coffee_rect = coffee.get_rect(midbottom=(780, 685))
 
 # Rectangle for the money_tab
 tab_rect = money_tab.get_rect(center=(1150, 90))
@@ -206,6 +227,12 @@ slash_rect = slash.get_rect(center = (395,410))
 mouse_rect2 = mouse.get_rect(center = (1050,490))
 t_rect = keyT.get_rect(center = (1125,490))
 slash_rect2 = slash.get_rect(center = (1085,490))
+
+#Start screen
+logo_rect = logo.get_rect(center=((displaySize[0] // 2)+50,( displaySize[1] // 2)))
+instructions_rect = instructions_surface.get_rect(midbottom =(logo_rect.bottomleft[0]+120, logo_rect.midbottom[1] + 100))
+start_rect = startKey.get_rect(midleft = (instructions_rect.midright[0]+40,instructions_rect.midright[1])) #Start screen key
+instructions_rect2 = instructions_surface2.get_rect(midleft = (start_rect.midright[0]+40,start_rect.midright[1]))
 
 # Detect Collision - flag bit
 collision_detected = False
@@ -242,8 +269,17 @@ rotateSunSpeed = 0.1
 rotateRange = 5
 rotateDirection = 1
 
+#Start Screen Flag
+
+START_SCREEN = 'start_screen'
+MAIN_GAME = 'main_game'
+game_state = START_SCREEN
+
 # Key timers
 keyTimer = 10000
+
+# Function to draw the start screen
+
 
 # Frames per second variable
 clock = pygame.time.Clock()
@@ -293,171 +329,185 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        elif event.type == pygame.VIDEORESIZE:
-            displaySize = event.size
-            screen = pygame.display.set_mode(displaySize, pygame.RESIZABLE)
-            surface = pygame.Surface(displaySize)
-            surface.fill('#87CEEB')
-            tab_rect = money_tab.get_rect(center=(displaySize[0] - 105, 90)) 
-            screen.blit(money_tab, tab_rect)
+        if game_state == START_SCREEN:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    game_state = MAIN_GAME
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if toaster_rect.collidepoint((event.pos)) and not collision_detected:
-                bread_flag, coffee_flag = True, False
-                collision_detected = True
-                bread_timer = current_time + BREAD_DISPLAY_TIME
-            elif coffeeM_rect.collidepoint((event.pos)) and not collision_detected:
-                coffee_flag, bread_flag = True, False
-                collision_detected = True
-                coffee_timer = current_time + COFFEE_DISPLAY_TIME
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:  # Reset collision detection flag on mouse button release
-                collision_detected = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_t:
-                bread_flag, coffee_flag = True, False
-                collision_detected = True
-                bread_timer = current_time + BREAD_DISPLAY_TIME
-            elif event.key == pygame.K_c:
-                coffee_flag, bread_flag = True, False
-                collision_detected = True
-                coffee_timer = current_time + COFFEE_DISPLAY_TIME
+        elif game_state == MAIN_GAME:
+            if event.type == pygame.VIDEORESIZE:
+                displaySize = event.size
+                screen = pygame.display.set_mode(displaySize, pygame.RESIZABLE)
+                surface = pygame.Surface(displaySize)
+                surface.fill('#87CEEB')
+                tab_rect = money_tab.get_rect(center=(displaySize[0] - 105, 90)) 
+                screen.blit(money_tab, tab_rect)
 
-    screen.blit(surface, (0, 0))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if toaster_rect.collidepoint((event.pos)) and not collision_detected:
+                    bread_flag, coffee_flag = True, False
+                    collision_detected = True
+                    bread_timer = current_time + BREAD_DISPLAY_TIME
+                elif coffeeM_rect.collidepoint((event.pos)) and not collision_detected:
+                    coffee_flag, bread_flag = True, False
+                    collision_detected = True
+                    coffee_timer = current_time + COFFEE_DISPLAY_TIME
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:  # Reset collision detection flag on mouse button release
+                    collision_detected = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_t:
+                    bread_flag, coffee_flag = True, False
+                    collision_detected = True
+                    bread_timer = current_time + BREAD_DISPLAY_TIME
+                elif event.key == pygame.K_c:
+                    coffee_flag, bread_flag = True, False
+                    collision_detected = True
+                    coffee_timer = current_time + COFFEE_DISPLAY_TIME
 
-    sunAngle += rotateSunSpeed * rotateDirection
-    if abs(sunAngle) >= rotateRange:
-        rotateDirection *= -1
+    if game_state == START_SCREEN:
+        screen.blit(logo, logo_rect)
+        screen.blit(instructions_surface, instructions_rect)
+        screen.blit(startKey,start_rect)
+        screen.blit(instructions_surface2, instructions_rect2)
 
-    # Rotate and blit the sun
-    rotated_sun = pygame.transform.rotozoom(sunSurface, sunAngle,1)
-    rotated_sun_rect = rotated_sun.get_rect(center=sun_rect.center)
-    screen.blit(rotated_sun, rotated_sun_rect)
+        pygame.display.update()
 
-    if current_cloud == 'cloud1':
-        screen.blit(cloudSurface, cloud_rect)
-    elif current_cloud == 'cloud2':
-        screen.blit(cloudSurface2, cloud2_rect)
-    elif current_cloud == 'cloud3':
-        screen.blit(cloudSurface3, cloud3_rect)
-    else:
-        screen.blit(cloudSurface4, cloud4_rect)
+    elif game_state == MAIN_GAME:
+        screen.blit(surface, (0, 0))
+        sunAngle += rotateSunSpeed * rotateDirection
+        if abs(sunAngle) >= rotateRange:
+            rotateDirection *= -1
 
-    # Check if it's time for a new person to appear
-    if current_time >= person_timer and not person_appeared:
-        current_person = randomly_select_person()
-        val = random_select_req()
-        person_appeared = True
-        person_timer = current_time + person_display_time
+        # Rotate and blit the sun
+        rotated_sun = pygame.transform.rotozoom(sunSurface, sunAngle,1)
+        rotated_sun_rect = rotated_sun.get_rect(center=sun_rect.center)
+        screen.blit(rotated_sun, rotated_sun_rect)
 
-    # Display the current person if appeared
-    if person_appeared:
-        screen.blit(request,req_rect)
-        if val == 'toast':
-            screen.blit(requesttoast,reqtost_rect)
-            target = 'toast'
+        if current_cloud == 'cloud1':
+            screen.blit(cloudSurface, cloud_rect)
+        elif current_cloud == 'cloud2':
+            screen.blit(cloudSurface2, cloud2_rect)
+        elif current_cloud == 'cloud3':
+            screen.blit(cloudSurface3, cloud3_rect)
         else:
-            screen.blit(requestcoffee,reqcof_rect)
-            target = 'coffee'
-        screen.blit(current_person, person_rect)
-        if person_rect.midbottom[1] > 715:
-            person_rect.centery -= 12
+            screen.blit(cloudSurface4, cloud4_rect)
 
-    screen.blit(tableSurface, table1_rect)
-    screen.blit(tableSurface2, table2_rect)
-    screen.blit(tableSurface3, table3_rect)
-    screen.blit(tableSurface4, table4_rect)
-    screen.blit(tableSurface5, table5_rect)
-    screen.blit(fridge, fridge_rect)
+        # Check if it's time for a new person to appear
+        if current_time >= person_timer and not person_appeared:
+            current_person = randomly_select_person()
+            val = random_select_req()
+            person_appeared = True
+            person_timer = current_time + person_display_time
 
-    # Handle bread display and person disappearance
-    if bread_flag and target == 'coffee':
-        if current_time < bread_timer:
-            screen.blit(wrong,wrong_rect)
+        # Display the current person if appeared
+        if person_appeared:
+            screen.blit(request,req_rect)
+            if val == 'toast':
+                screen.blit(requesttoast,reqtost_rect)
+                target = 'toast'
+            else:
+                screen.blit(requestcoffee,reqcof_rect)
+                target = 'coffee'
+            screen.blit(current_person, person_rect)
+            if person_rect.midbottom[1] > 715:
+                person_rect.centery -= 12
+
+        screen.blit(tableSurface, table1_rect)
+        screen.blit(tableSurface2, table2_rect)
+        screen.blit(tableSurface3, table3_rect)
+        screen.blit(tableSurface4, table4_rect)
+        screen.blit(tableSurface5, table5_rect)
+        screen.blit(fridge, fridge_rect)
+
+        # Handle bread display and person disappearance
+        if bread_flag and target == 'coffee':
+            if current_time < bread_timer:
+                screen.blit(wrong,wrong_rect)
+                bread_rect.centery = bread_initial_y + int(bread_float_offset)
+                screen.blit(bread, bread_rect)
+        elif bread_flag:
             bread_rect.centery = bread_initial_y + int(bread_float_offset)
             screen.blit(bread, bread_rect)
-    elif bread_flag:
-        bread_rect.centery = bread_initial_y + int(bread_float_offset)
-        screen.blit(bread, bread_rect)
-        if target == 'toast'and coin_rect.centery > 180:
-            screen.blit(plus,plus_rect)
-            screen.blit(coin,coin_rect)
-            coin_rect.centery -= 2
-            plus_rect.centery -= 2
-        if current_time > bread_timer and target == 'toast':
-            person_appeared = False
-            bread_flag = False
-            score += 2
-            coin_rect.centery = 300
-            plus_rect.midright = (posPlus[0]-5,posPlus[1])
-            person_rect.midbottom = (780,900)
-        elif current_time < bread_timer and target == 'toast':
-            screen.blit(correct,correct_rect)
+            if target == 'toast'and coin_rect.centery > 180:
+                screen.blit(plus,plus_rect)
+                screen.blit(coin,coin_rect)
+                coin_rect.centery -= 2
+                plus_rect.centery -= 2
+            if current_time > bread_timer and target == 'toast':
+                person_appeared = False
+                bread_flag = False
+                score += 2
+                coin_rect.centery = 300
+                plus_rect.midright = (posPlus[0]-5,posPlus[1])
+                person_rect.midbottom = (780,900)
+            elif current_time < bread_timer and target == 'toast':
+                screen.blit(correct,correct_rect)
 
-    # Handle coffee display and person disappearance
-    if coffee_flag and target == 'toast':
-        if current_time < coffee_timer:
-            screen.blit(wrong,wrong_rect)
+        # Handle coffee display and person disappearance
+        if coffee_flag and target == 'toast':
+            if current_time < coffee_timer:
+                screen.blit(wrong,wrong_rect)
+                coffee_rect.centery = coffee_initial_y + int(coffee_float_offset)
+                screen.blit(coffee, coffee_rect)
+        elif coffee_flag:
             coffee_rect.centery = coffee_initial_y + int(coffee_float_offset)
             screen.blit(coffee, coffee_rect)
-    elif coffee_flag:
-        coffee_rect.centery = coffee_initial_y + int(coffee_float_offset)
-        screen.blit(coffee, coffee_rect)
-        if target == 'coffee' and coin_rect.centery > 180:
-            screen.blit(plus,plus_rect)
-            screen.blit(coin,coin_rect)
-            coin_rect.centery -= 2
-            plus_rect.centery -= 2
-        if current_time > coffee_timer and target == 'coffee':
-            person_appeared = False
-            coffee_flag = False
-            score += 5 
-            coin_rect.centery = 300
-            plus_rect.midright = (posPlus[0]-5,posPlus[1])
-            person_rect.midbottom = (780,900)
-        elif current_time < coffee_timer and target == 'coffee':
-            screen.blit(correct,correct_rect)
+            if target == 'coffee' and coin_rect.centery > 180:
+                screen.blit(plus,plus_rect)
+                screen.blit(coin,coin_rect)
+                coin_rect.centery -= 2
+                plus_rect.centery -= 2
+            if current_time > coffee_timer and target == 'coffee':
+                person_appeared = False
+                coffee_flag = False
+                score += 5 
+                coin_rect.centery = 300
+                plus_rect.midright = (posPlus[0]-5,posPlus[1])
+                person_rect.midbottom = (780,900)
+            elif current_time < coffee_timer and target == 'coffee':
+                screen.blit(correct,correct_rect)
 
-    screen.blit(toast, toaster_rect)
-    screen.blit(coffeemaker, coffeeM_rect)
+        screen.blit(toast, toaster_rect)
+        screen.blit(coffeemaker, coffeeM_rect)
 
-    # Blink the mouse and keys
-    if keyTimer > 0:
-        if blink_state:
-            screen.blit(mouse,mouse_rect)
-            screen.blit(mouse,mouse_rect2)
-            screen.blit(keyC,c_rect)
-            screen.blit(keyT,t_rect)
-            screen.blit(slash,slash_rect)
-            screen.blit(slash,slash_rect2)
-        keyTimer -= 30
+        # Blink the mouse and keys
+        if keyTimer > 0:
+            if blink_state:
+                screen.blit(mouse,mouse_rect)
+                screen.blit(mouse,mouse_rect2)
+                screen.blit(keyC,c_rect)
+                screen.blit(keyT,t_rect)
+                screen.blit(slash,slash_rect)
+                screen.blit(slash,slash_rect2)
+            keyTimer -= 30
 
-    if current_cloud == 'cloud1':
-        cloud_rect.centerx -= 2
-        if cloud_rect.right <= 0:
-            current_cloud = randomly_select_cloud()
-            cloud_rect = cloudSurface.get_rect(center=(2000, random.randint(50, 150)))
-    elif current_cloud == 'cloud2':
-        cloud2_rect.centerx -= 2
-        if cloud2_rect.right <= 0:
-            current_cloud = randomly_select_cloud()
-            cloud2_rect = cloudSurface2.get_rect(center=(2000, random.randint(50, 150)))
-    elif current_cloud == 'cloud3':
-        cloud3_rect.centerx -= 2
-        if cloud3_rect.right <= 0:
-            current_cloud = randomly_select_cloud()
-            cloud3_rect = cloudSurface3.get_rect(center=(2000, random.randint(50, 150)))
-    elif current_cloud == 'cloud4':
-        cloud4_rect.centerx -= 2
-        if cloud4_rect.right <= 0:
-            current_cloud = randomly_select_cloud()
-            cloud4_rect = cloudSurface4.get_rect(center=(2000, random.randint(50, 150)))
+        if current_cloud == 'cloud1':
+            cloud_rect.centerx -= 2
+            if cloud_rect.right <= 0:
+                current_cloud = randomly_select_cloud()
+                cloud_rect = cloudSurface.get_rect(center=(2000, random.randint(50, 150)))
+        elif current_cloud == 'cloud2':
+            cloud2_rect.centerx -= 2
+            if cloud2_rect.right <= 0:
+                current_cloud = randomly_select_cloud()
+                cloud2_rect = cloudSurface2.get_rect(center=(2000, random.randint(50, 150)))
+        elif current_cloud == 'cloud3':
+            cloud3_rect.centerx -= 2
+            if cloud3_rect.right <= 0:
+                current_cloud = randomly_select_cloud()
+                cloud3_rect = cloudSurface3.get_rect(center=(2000, random.randint(50, 150)))
+        elif current_cloud == 'cloud4':
+            cloud4_rect.centerx -= 2
+            if cloud4_rect.right <= 0:
+                current_cloud = randomly_select_cloud()
+                cloud4_rect = cloudSurface4.get_rect(center=(2000, random.randint(50, 150)))
 
-    screen.blit(money_tab, tab_rect)
-    # Display score
-    score_text = custom_font.render(f"$ {score}", True, (0, 0, 0))  # Adjust text color as needed
-    score_text_rect = score_text.get_rect(center=(tab_rect.centerx, tab_rect.centery-2))
-    screen.blit(score_text, score_text_rect)
+        screen.blit(money_tab, tab_rect)
+        # Display score
+        score_text = custom_font.render(f"$ {score}", True, (0, 0, 0))  # Adjust text color as needed
+        score_text_rect = score_text.get_rect(center=(tab_rect.centerx, tab_rect.centery-2))
+        screen.blit(score_text, score_text_rect)
 
-    pygame.display.update()
-    clock.tick(60)
+        pygame.display.update()
+        clock.tick(60)
